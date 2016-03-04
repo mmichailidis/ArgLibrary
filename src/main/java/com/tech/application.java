56f077259e4e8d0d5089312de9da115d2dfaf43f
newbi12 <shinigamidxd@gmail.com>
2016-03-04 23:30:52 +0200

    Initial Commit

---
+++ src/main/java/com/tech/application.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tech;
+
+/**
+ *
+ * @author KuroiTenshi
+ */
+public class application {
+    public static void main(String[] args){
+        
+    }
+}
